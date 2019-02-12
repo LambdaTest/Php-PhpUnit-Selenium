ca7c303865e79563ac4a74c829286592f944d5d5
hp <ankitp@lambdatest.com>
2019-02-12 21:27:52 +0530

    Sample Tests

--- tests/LambdaTestParallel.php
+++ tests/LambdaTestParallel.php
@@ -72,7 +72,7 @@ public function setupLambdatest($bName, $VName, $osName)
        		}
 	    
 		catch(Exception $e){
-		echo "Error ".$e->getTraceAsString();
+		echo "ErrorIN ".$e->getTraceAsString();
 		self::$driver->executeScript("lambda-status=failed");
 		}
 	}
